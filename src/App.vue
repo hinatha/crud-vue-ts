7aedf73bb8d49d13c514516cff0e19e31ce9dcb2
hinatha <hinatha0110@gmail.com>
2022-11-30 19:13:47 +0900

    App.vue 修正

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, provide } from 'vue'
+import TodoStore, { todoKey } from '@/store/todo'
 
 export default defineComponent({
   name: 'App',
+  // Provide store for global use
+  setup () {
+    // provide(store's key, store's value)
+    provide(todoKey, TodoStore)
+  },
 })
 </script>
