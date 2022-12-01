86d002c5c5fe661b0b0f08662438f6a10e463465
hinatha <hinatha0110@gmail.com>
2022-12-01 14:42:00 +0900

    todos componentをrouterに追加

--- src/views/todos.vue
+++ src/views/todos.vue
@@ -1,17 +1,1 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Todos from '@/views/todos.vue'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Todos',
-    component: Todos,
-  },
-]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-})
-
-export default router
+<template>TODO一覧です。</template>
