d7fa77cce1c45a44f4125d2fed9fdcaab2698d3e
hinatha <hinatha0110@gmail.com>
2022-12-01 17:12:10 +0900

    Todo一覧においてTodoItemコンポーネントを利用するように修正

--- src/views/todo.vue
+++ src/views/todo.vue
@@ -1,21 +1,31 @@
 <template>
   <h2>TODO一覧</h2>
   <ul>
-    <!-- const state = reactive<TodoState> ({ todos: mockTodo, }) -->
-    <!-- state: readonly(state), -->
-    <li v-for="todo in todoStore.state.todos" :key="todo.id">
-      {{ todo.title }}
-    </li>
-    <!-- Link for AddTodo page -->
-    <router-link to="/new">新規作成</router-link>
+    <!-- :todo pass data to child component -->
+    <!-- emit need to change camel case to kebab case (clickTitle -> click-title) -->
+    <todo-item
+      v-for="todo in todoStore.state.todos"
+      :key="todo.id"
+      :todo="todo"
+      @click-title="clickTitle"
+      @click-delete="clickDelete"
+    >
+    </todo-item>
   </ul>
+  <!-- Link for AddTodo page -->
+  <router-link to="/new">新規作成</router-link>
 </template>
 
 <script lang="ts">
 import { defineComponent, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import TodoItem from '@/components/TodoItem.vue'
 import { todoKey } from '@/store/todo'
 
 export default defineComponent({
+  components: {
+    TodoItem,
+  },
   setup () {
     // Inject todoStore
     const todoStore = inject(todoKey)
@@ -25,8 +35,28 @@ export default defineComponent({
       throw new Error('todoStore is not provided')
     }
 
+    // Call useRouter() to get the routes
+    // Former vue.js can access the routes by this.$router
+    // In case of composition api can't access to "this"
+    const router = useRouter()
+
+    // Parent component can receive props.todo.item as the event argument
+    // Set todo id as argument of this method
+    const clickDelete = (id: number) => {
+      todoStore.deleteTodo(id)
+    }
+
+    // Parent component can receive props.todo.item as the event argument
+    // Set todo id as argument of this method
+    const clickTitle = (id: number) => {
+      // Move to edit page
+      router.push(`/edit/${id}`)
+    }
+
     return {
       todoStore,
+      clickDelete,
+      clickTitle,
     }
   },
 })
